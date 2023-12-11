b80ca70b3dbdb7b489e02529c3fbbf21bf8c828f
Julien Dané <julien.Dane@gmail.com>
2023-12-11 16:07:42 +0100

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { initializeFirebase } from "./firebase.ts";
+import "./index.css";
+
+initializeFirebase();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
